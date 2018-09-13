92e00632e3333db19c3814faafbff5f9154b04b1
Eugene Danilovets <evgeniy.danilovets@gmail.com>
2018-09-13 17:20:48 +0300

    add tests in test create customer

--- frontend/application/app.py
+++ frontend/application/app.py
@@ -1,11 +1,17 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+import json
 
 
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Chrome()
+
+    def load_config(self, file):
+        with open(file, 'r') as f:
+            config = json.load(f)
+        return config
 
     def open_home_page(self):
         wd = self.wd
@@ -19,17 +25,27 @@ class Application:
             return False
         return True
 
+    def check_exists_by_xpath(self, xpath_selector):
+        wd = self.wd
+        try:
+            wd.find_element_by_xpath(xpath_selector)
+        except NoSuchElementException:
+            return False
+        return True
+
     def create_customer(self, customer, subscribe=False):
         wd = self.wd
         self.open_home_page()
-        wd.find_element_by_xpath("//div[@id='top-links']/ul/li[2]/a").click()
+        xpath = self.load_config("xpath.json")
+
+        wd.find_element_by_xpath(xpath['top_line']['my_account']).click()
 
         if self.check_exists_by_link_text("Register"):
-            wd.find_element_by_link_text("Register").click()
+            wd.find_element_by_xpath(xpath['top_line']['register']).click()
         elif self.check_exists_by_link_text("Logout"):
-            wd.find_element_by_link_text("Logout").click()
-            wd.find_element_by_xpath("//div[@id='top-links']/ul/li[2]/a").click()
-            wd.find_element_by_link_text("Register").click()
+            wd.find_element_by_xpath(xpath['top_line']['logout']).click()
+            wd.find_element_by_xpath(xpath['top_line']['my_account']).click()
+            wd.find_element_by_xpath(xpath['top_line']['register']).click()
         else:
             raise NoSuchElementException("Cannot find Register/Logout links. Check you UI.")
 
@@ -46,10 +62,40 @@ class Application:
         type_by_name("confirm", customer.password)
 
         if subscribe:
-            wd.find_element_by_css_selector("input[name=newsletter]").click()
+            wd.find_element_by_xpath(xpath['account']['subscribe_yes']).click()
 
-        wd.find_element_by_name("agree").click()
-        wd.find_element_by_css_selector("input.btn.btn-primary").click()
+        wd.find_element_by_xpath(xpath['account']['checkbox_agree']).click()
+        wd.find_element_by_xpath(xpath['account']['btn_continue']).click()
+
+    def is_warning_message_showed(self, field_name):
+        wd = self.wd
+        xpath = self.load_config("xpath.json")
+        if field_name == "Password":
+            warn_message = wd.find_element_by_xpath(xpath['account']['warning_password']).text
+            if warn_message == "Password must be between 4 and 20 characters!":
+                return True
+        elif field_name == "Password Confirm":
+            warn_message = wd.find_element_by_xpath(xpath['account']['warning_confirm']).text
+            if warn_message == "Password confirmation does not match password!":
+                return True
+        elif field_name == "First Name":
+            warn_message = wd.find_element_by_xpath(xpath['account']['warning_firstname']).text
+            if warn_message == "First Name must be between 1 and 32 characters!":
+                return True
+        elif field_name == "Last Name":
+            warn_message = wd.find_element_by_xpath(xpath['account']['warning_lastname']).text
+            if warn_message == "Last Name must be between 1 and 32 characters!":
+                return True
+        elif field_name == "E-mail":
+            warn_message = wd.find_element_by_xpath(xpath['account']['warning_email']).text
+            if warn_message == "E-Mail Address does not appear to be valid!":
+                return True
+        elif field_name == "Telephone":
+            warn_message = wd.find_element_by_xpath(xpath['account']['warning_phone']).text
+            if warn_message == "Telephone must be between 3 and 32 characters!":
+                return True
+        else:
+            return False
 
     def destroy(self):
         self.wd.quit()
--- frontend/tests/test_create_customer.py
+++ frontend/tests/test_create_customer.py
@@ -10,20 +10,27 @@ def random_password(max_len):
     return "".join([random.choice(symbols) for _ in range(max_len)])
 
 
-test_data_sample = [
+# test data for subscribe: Yes or No
+test_data_subscribe = [
     Customer("John", "Travolta", "john.email@gmail.com", "+380989990066", "111111")
 ]
 
-# test data for password from 0 to 20 character
+# test data for password from 4 to 20 character
 test_data_password = [
     Customer("John", "Travolta", "john.email@gmail.com", "+380989990066", "{}".format(random_password(i)))
-    for i in range(21)
+    for i in range(4, 21)
+]
+
+# test data for password negative
+test_data_password_negative = [
+    Customer("John", "Travolta", "john.email@gmail.com", "+380989990066", "{}".format(random_password(i)))
+    for i in range(3)
 ]
 
 
-@pytest.mark.parametrize("new_customer", test_data_sample, ids=[repr(x) for x in test_data_sample])
 @pytest.mark.parametrize("subscribe", [True, False], ids=["Subscribe=Yes", "Subscribe=No"])
-def test_create_customer_subscribe(app, db, new_customer, subscribe):
+@pytest.mark.parametrize("new_customer", test_data_subscribe, ids=[repr(x) for x in test_data_subscribe])
+def test_create_customer_subscribe_positive(app, db, new_customer, subscribe):
     app.create_customer(new_customer, subscribe)
     customers = db.get_customers_list()
     assert new_customer in customers
@@ -33,7 +40,7 @@ def test_create_customer_subscribe(app, db, new_customer, subscribe):
 
 
 @pytest.mark.parametrize("new_customer", test_data_password, ids=[repr(x) for x in test_data_password])
-def test_create_customer_password(app, db, new_customer):
+def test_create_customer_password_positive(app, db, new_customer):
     app.create_customer(new_customer)
     customers = db.get_customers_list()
     assert new_customer in customers
@@ -42,4 +49,15 @@ def test_create_customer_password(app, db, new_customer):
     db.delete_customer_by_name_and_lastname(new_customer)
 
 
+@pytest.mark.parametrize("new_customer", test_data_password_negative, ids=[repr(x) for x in test_data_password_negative])
+def test_create_customer_password_negative(app, new_customer):
+    app.create_customer(new_customer)
+    assert app.is_warning_message_showed("Password")
 
+
+def test_create_customer_personal_info_empty(app):
+    app.create_customer(Customer("", "", "", "", ""))
+    assert app.is_warning_message_showed("First Name")
+    assert app.is_warning_message_showed("Last Name")
+    assert app.is_warning_message_showed("E-mail")
+    assert app.is_warning_message_showed("Telephone")
