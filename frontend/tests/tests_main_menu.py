810363e3b3d22f6cb5aad6e955d71b9c3710bbea
Eugene Danilovets <evgeniy.danilovets@gmail.com>
2018-10-09 18:04:36 +0300

    remove tests folder

--- frontend/tests/tests_main_menu.py
+++
@@ -1,9 +0,0 @@
-def test_menu_links_open_correct_pages(app):
-    expected_headers = ['PC', 'Mac', 'Macs', 'Windows', 'Mice and Trackballs', 'Monitors', 'Printers', 'Scanners',
-                        'Web Cameras', 'Tablets', 'Software', 'Phones & PDAs', 'Cameras', 'test 11', 'test 12',
-                        'test 15', 'test 16', 'test 17', 'test 18', 'test 19', 'test 20', 'test 21', 'test 22',
-                        'test 23', 'test 24', 'test 4', 'test 5', 'test 6', 'test 7', 'test 8', 'test 9']
-    content_headers = app.get_content_headers()
-    print("\nActual content headers:\n{}".format(content_headers))
-    # expected_headers = db.get_content_headers()
-    assert content_headers == expected_headers
